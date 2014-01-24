99bb127bc4ae042032b4079670d2b4ef418800c3
Mona Chitnis <chitnis@yahoo-inc.com>
2014-01-23 20:11:02 -0800

    match func and unit tests updated

--- src/org/apache/pig/newplan/logical/rules/NestedForEachUserFunc.java
+++ src/org/apache/pig/newplan/logical/rules/NestedForEachUserFunc.java
@@ -1,8 +1,12 @@
 package org.apache.pig.newplan.logical.rules;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.pig.impl.logicalLayer.FrontendException;
@@ -16,8 +20,12 @@ import org.apache.pig.newplan.logical.expression.ProjectExpression;
 import org.apache.pig.newplan.logical.expression.UserFuncExpression;
 import org.apache.pig.newplan.logical.relational.LOForEach;
 import org.apache.pig.newplan.logical.relational.LOGenerate;
+import org.apache.pig.newplan.logical.relational.LOInnerLoad;
+import org.apache.pig.newplan.logical.relational.LOLoad;
 import org.apache.pig.newplan.logical.relational.LogicalPlan;
 import org.apache.pig.newplan.logical.relational.LogicalRelationalOperator;
+import org.apache.pig.newplan.logical.relational.LogicalSchema;
+import org.apache.pig.newplan.logical.relational.LogicalSchema.LogicalFieldSchema;
 import org.apache.pig.newplan.optimizer.Rule;
 import org.apache.pig.newplan.optimizer.Transformer;
 
@@ -44,13 +52,17 @@ public class NestedForEachUserFunc extends Rule {
     public class NestedForEachUserFuncTransformer extends Transformer {
 
         LOForEach foreach = null;
-        Set<Pair<Pair<Long, Integer>, UserFuncExpression>> userFuncSet;
-        Iterator<Pair<Pair<Long, Integer>, UserFuncExpression>> listIter;
+        Set<Pair<Pair<Integer, Integer>, UserFuncExpression>> userFuncSet;
+        Map<LogicalExpressionPlan, Integer> userFuncMap;
+        Iterator<Pair<Pair<Integer, Integer>, UserFuncExpression>> setIter;
+        Iterator<Entry<LogicalExpressionPlan, Integer>> mapIter;
+        Set<Long> projectedColumns;
         OperatorSubPlan subPlan = null;
+        LogicalExpressionPlan userFuncPlan = null;
 
         @Override
         public boolean check(OperatorPlan matched) throws FrontendException {
-            boolean nested = false;
+            boolean ret = false;
             Iterator<Operator> iter = matched.getOperators();
             while (iter.hasNext()) {
                 Operator op = iter.next();
@@ -59,79 +71,168 @@ public class NestedForEachUserFunc extends Rule {
                     break;
                 }
             }
-            // 1. infer nested foreach
-            OperatorPlan innerPlan = foreach.getPlan();
-            Operator pred = innerPlan.getPredecessors(foreach).get(0);
 
-            if (pred instanceof LOForEach) {
-                nested = true;
+            List<Operator> sources = foreach.getPlan().getSources();
+            for (Operator source : sources) {
+                LOLoad load = (LOLoad) source;
+                LogicalSchema schema = load.getSchema();
+                if (schema != null) {
+                    if (projectedColumns == null) {
+                        projectedColumns = new HashSet<Long>();
+                    }
+                    List<LogicalFieldSchema> fields = schema.getFields();
+                    for (LogicalFieldSchema field : fields) {
+                        projectedColumns.add(field.uid);
+                    }
+                }
             }
-            // 2. traverse and record userfunc instances and count
-            if (nested) {
-                OperatorPlan inner = foreach.getInnerPlan();
-                Iterator<Operator> iter2 = inner.getOperators();
-                while (iter2.hasNext()) {
-                    Operator op = iter2.next();
-                    if (op instanceof LOGenerate) {
-                        List<LogicalExpressionPlan> generatePlan = ((LOGenerate) op).getOutputPlans();
-                        Iterator<LogicalExpressionPlan> lepIter = generatePlan.iterator();
-                        while (lepIter.hasNext()) {
-                            Iterator<Operator> expIter = lepIter.next().getOperators();
-                            while (expIter.hasNext()) {
-                                Operator expOp = expIter.next();
-                                if (expOp instanceof UserFuncExpression) {
-                                    Operator column = expOp.getPlan().getSuccessors(expOp).get(0);
-                                    long col = ((ProjectExpression)column).getColNum();
-                                    if (userFuncSet == null) {
-                                        userFuncSet = new HashSet<Pair<Pair<Long, Integer>, UserFuncExpression>>();
-                                    }
-                                    listIter = userFuncSet.iterator();
-                                    while (listIter.hasNext()) {
-                                        Pair<Pair<Long, Integer>, UserFuncExpression> func = listIter.next();
-                                        if (func.second == expOp) {
-                                            func.first.second++;
-                                        }
-                                    }
-                                    userFuncSet.add(new Pair(new Pair(col, 0), expOp));
-                                }
-                                break;
+
+            // traverse and record userfunc instances and count
+            // if (nested) {
+            LOGenerate genOp = (LOGenerate) foreach.getInnerPlan().getSinks().get(0);
+            List<LogicalExpressionPlan> generatePlan = genOp.getOutputPlans();
+            Iterator<LogicalExpressionPlan> genIter = generatePlan.iterator();
+            while (genIter.hasNext()) {
+                Iterator<Operator> genOps = genIter.next().getOperators();
+                Set<Long> visited = new HashSet<Long>();
+                while (genOps.hasNext()) {
+                    Operator expOp = genOps.next();
+                    // check for nested userfunc and its operands
+                    if (checkNestedUserFunc((LogicalExpression) expOp, visited) && userFuncPlan != null) {
+                        if (userFuncMap == null) {
+                            userFuncMap = new HashMap<LogicalExpressionPlan, Integer>();
+                        }
+                        mapIter = userFuncMap.entrySet().iterator();
+                        while (mapIter.hasNext()) {
+                            if (mapIter.next().getKey().isEqual(userFuncPlan)) {
+                                int occ = userFuncMap.get(userFuncPlan);
+                                occ++;
+                                continue;
                             }
                         }
-                        break;
-                    }
-                }
-                if (userFuncSet != null && !userFuncSet.isEmpty()) {
-                    listIter = userFuncSet.iterator();
-                    while (listIter.hasNext()) {
-                        Pair<Pair<Long, Integer>, UserFuncExpression> func = listIter.next();
-                        if (func.first.second > 1) {
-                            // even if true for one, match is overall true
-                            return true;
-                        }
+                        userFuncMap.put(userFuncPlan, 1);
+                        userFuncPlan = null;
+
                     }
                 }
             }
-            return false;
+            return ret;
         }
+
+        private boolean checkNestedUserFunc(LogicalExpression exp, Set visited) throws FrontendException {
+            boolean top = false;
+            long uid = exp.getFieldSchema().uid;
+            if (exp instanceof UserFuncExpression && !visited.contains(uid)) {
+                if (userFuncPlan == null) {
+                    userFuncPlan = new LogicalExpressionPlan();
+                    userFuncPlan.add((UserFuncExpression)exp);
+                    top = true;  //top-level
+                }
+                List<LogicalExpression> userFuncOperands = ((UserFuncExpression) exp).getArguments();
+                for (LogicalExpression ufo : userFuncOperands) {
+                    if(!checkNestedUserFunc(ufo, visited)) {
+                        return false;
+                    }
+                }
+                visited.add(uid);
+            }
+            else if (exp instanceof ProjectExpression) {
+                if (!projectedColumns.contains(exp.getFieldSchema().uid)) {
+                    // valid column among initial projections
+                    // and not from internal nested block
+                    return false;
+                }
+            }
+            if (!top && userFuncPlan != null) {
+                userFuncPlan.add(exp);
+                userFuncPlan.connect(((LogicalExpressionPlan)exp.getPlan()).getPredecessors(exp).get(0), exp);
+            }
+            return true;
+        }
+
+        /*private LogicalExpressionPlan getToPlan(Operator op) {
+            if (op == dupe) {
+                return (LogicalExpressionPlan) op.getPlan();
+            }
+            else {
+                LogicalExpressionPlan lep = (LogicalExpressionPlan)op.getPlan();
+                return getToPlan(((LogicalExpressionPlan)op.getPlan()).getPredecessors(op).get(0));
+            }
+        }*/
 
         @Override
         public void transform(OperatorPlan matched) throws FrontendException {
+
             LogicalPlan innerPlan = foreach.getInnerPlan();
-            List<Operator> sinks1 = innerPlan.getSinks();
+            // for splitting into two foreach stages, create a new foreach
+            // operator
+            LOForEach newForeach = new LOForEach(innerPlan);
+            LogicalPlan newInnerPlan = new LogicalPlan();
+            newForeach.setInnerPlan(newInnerPlan);
+            newForeach.setAlias(foreach.getAlias());
+            List<LogicalExpressionPlan> exps = new ArrayList<LogicalExpressionPlan>();
+            LOGenerate newGenerate = new LOGenerate(newInnerPlan, exps, new boolean[1]); // TODO
+                                                                                         // index?
+            newInnerPlan.add(newGenerate);
+            LOInnerLoad newInnerLoad = new LOInnerLoad(newInnerPlan, foreach, 0); // <--
+            newInnerPlan.add(newInnerLoad);
+            newInnerPlan.connect(newInnerLoad, newGenerate);
+            System.out.println(newInnerPlan);
+
+            // populate the project expressions under new foreach -- generate
+            // iterate through userfuncset
+            setIter = userFuncSet.iterator();
+            while (setIter.hasNext()) {
+                Pair<Pair<Integer, Integer>, UserFuncExpression> item = setIter.next();
+                if (item.first.second > 1) {
+                    ProjectExpression prx = new ProjectExpression(newGenerate.getPlan(), 0, item.first.first,
+                            newGenerate);
+                    // can be multiple projects
+                    LogicalExpressionPlan lexPlan = new LogicalExpressionPlan();
+                    lexPlan.add(item.second);
+                    // lexPlan.add(prx);
+                    lexPlan.connect(item.second, prx);
+                    exps.add(lexPlan);
+                }
+            }
+
+            // disconnect UserFunc from BinCond
+            LOGenerate existingGen = (LOGenerate) innerPlan.getSinks().get(0);
+            List<LogicalExpressionPlan> generatePlan = existingGen.getOutputPlans();
+            Iterator<LogicalExpressionPlan> genIter = generatePlan.iterator();
+            while (genIter.hasNext()) {
+                LogicalExpressionPlan genPlan = genIter.next();
+                Iterator<Operator> opIter = genPlan.getOperators();
+                while (opIter.hasNext()) {
+                    Operator expOp = opIter.next();
+                    if (expOp instanceof UserFuncExpression) {
+                        Operator column = expOp.getPlan().getSuccessors(expOp).get(0);
+                        int col = (int) ((ProjectExpression) column).getFieldSchema().uid;
+                        setIter = userFuncSet.iterator();
+                        while (setIter.hasNext()) {
+                            Pair<Pair<Integer, Integer>, UserFuncExpression> item = setIter.next();
+                            if (item.second.getName() == expOp.getName() && item.first.first == col) {
+                                genPlan.disconnect(genPlan.getPredecessors(expOp).get(0), expOp);
+                                currentPlan.remove(expOp);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+
+            /*
+             * //disconnect older unoptimized innerLoads for (Operator oldLoad :
+             * innerPlan.getSources()) { innerPlan.disconnect(existingGen,
+             * oldLoad); //currentPlan.remove? disconnect? }
+             */
+
+            // add new foreach as predecessor to existing foreach
+            currentPlan.insertBetween(foreach, newForeach, currentPlan.getSuccessors(foreach).get(0));
             subPlan = new OperatorSubPlan(currentPlan);
-            LogicalExpression userfunc = null;
-
-            LogicalRelationalOperator foreach1 = new LOForEach(innerPlan);
-            subPlan.add(foreach1);
-            currentPlan.connect(foreach, foreach1);
-            currentPlan.connect(foreach1, userfunc);
-
-            LogicalRelationalOperator foreach2 = new LOForEach(innerPlan);
-            subPlan.add(foreach2);
-            // get the first sink from foreach --> BinCond
-            Operator expOp = sinks1.get(0);
-            currentPlan.connect(foreach, foreach2);
-            currentPlan.connect(foreach2, expOp);
+            subPlan.add(newForeach);
+            subPlan.add(newGenerate);
+            subPlan.add(newInnerLoad);
 
         }
 
--- test/org/apache/pig/test/TestNestedForEachUserFunc.java
+++ test/org/apache/pig/test/TestNestedForEachUserFunc.java
@@ -1,16 +1,133 @@
 package org.apache.pig.test;
 
+import java.util.Iterator;
 import java.util.Properties;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.pig.ExecType;
-import org.apache.pig.impl.PigContext;
+import org.apache.pig.PigServer;
+import org.apache.pig.data.Tuple;
 import org.junit.Test;
 
-public class TestNestedForEachUserFunc {
-    PigContext pc = new PigContext(ExecType.LOCAL, new Properties());
+public class TestNestedForEachUserFunc extends junit.framework.TestCase {
+    private PigServer pig;
+
+    public TestNestedForEachUserFunc() throws Exception {
+        pig = new PigServer(ExecType.LOCAL, new Properties());
+        pig.setValidateEachStatement(true);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        String[] input = {"1\tFrodo\tBaggins\tHobbbit", "2\tSam\tGamgee\tHobbit", "3\tGandalf\tGrey\tWizard"};
+        Util.createInputFile(FileSystem.getLocal(new Configuration()), "my_data", input);
+
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        FileSystem.getLocal(new Configuration()).delete(new Path("my_data"), true);
+    }
 
     @Test
     public void testSimple() throws Exception {
 
+        pig.registerQuery("a = load 'my_data' as (col1:int, col2:chararray, col3:chararray, col4:chararray);\n");
+        pig.registerQuery("b = foreach a { c1 = UPPER(col2); generate (c1 eq 'FRODO' ? 1 : 0), (c1 eq 'SAM' ? 2 : 0); }\n");
+
+        Iterator<Tuple> iter = pig.openIterator("b");
+        String[] expected = new String[]{"(1,0)", "(0,2)", "(0,0)"};
+
+        Util.checkQueryOutputsAfterSortRecursive(iter, expected,
+                org.apache.pig.newplan.logical.Util.translateSchema(pig.dumpSchema("b")));
+
     }
+
+    /*@Test
+    public void testComplex() throws Exception {
+
+        pig.registerQuery("a = load 'my_data' as (col1:int, col2:chararray, col3:chararray, col4:chararray);\n");
+        pig.registerQuery("b = foreach a { x = TOBAG(TOTUPLE(col1, col2));"
+                                        + "y = UPPER(col3); "
+                                        + "generate col1, (x.$1.$0 eq 'Frodo' ? 1 : 0), (x.$1.$0 eq 'Sam' ? 2 : 0), y, LOWER(col4); }\n");
+
+        Iterator<Tuple> iter = pig.openIterator("b");
+        String[] expected = new String[]{"(1,1,0,BAGGINS,hobbit)",
+                                         "(2,0,2,GAMGEE,hobbit)",
+                                         "(3,0,0,GREY,wizard)"};
+
+        Util.checkQueryOutputsAfterSortRecursive(iter, expected,
+                org.apache.pig.newplan.logical.Util.translateSchema(pig.dumpSchema("b")));
+
+    }
+*/
+    @Test
+    public void testCastWithinUserFunc() throws Exception {
+
+        pig.registerQuery("a = load 'my_data' as (col1, col2, col3, col4);\n");
+        pig.registerQuery("b = foreach a { c1 = UPPER((chararray)col2); generate (c1 eq 'FRODO' ? 1 : 0), (c1 eq 'SAM' ? 2 : 0); }\n");
+
+        Iterator<Tuple> iter = pig.openIterator("b");
+        String[] expected = new String[]{"(1,0)", "(0,2)", "(0,0)"};
+
+        Util.checkQueryOutputsAfterSortRecursive(iter, expected,
+                org.apache.pig.newplan.logical.Util.translateSchema(pig.dumpSchema("b")));
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+
+        pig.registerQuery("a = load 'my_data' as (col1:int, col2:chararray, col3:chararray, col4:chararray);\n");
+        pig.registerQuery("b = foreach a { c1 = UPPER($1); generate (c1 eq 'FRODO' ? 1 : 0), (c1 eq 'SAM' ? 2 : 0), LOWER($2); }\n");
+
+        Iterator<Tuple> iter = pig.openIterator("b");
+        String[] expected = new String[]{"(1,0,baggins)", "(0,2,gamgee)", "(0,0,grey)"};
+
+        Util.checkQueryOutputsAfterSortRecursive(iter, expected,
+                org.apache.pig.newplan.logical.Util.translateSchema(pig.dumpSchema("b")));
+    }
+
+    @Test
+    public void testScalars() throws Exception {
+
+        String[] input = {"4\tAragorn\tII\tMan"};
+        Util.createInputFile(FileSystem.getLocal(new Configuration()), "one_data", input);
+
+        pig.registerQuery("a = load 'my_data' as (a1,a2,a3,a4);\n");
+        pig.registerQuery("b = load 'one_data' as (b1,b2,b3,b4);\n");
+        pig.registerQuery("c = foreach a { c1 = CONCAT((chararray)a2, (chararray)b.b2); generate c1, (c1 eq 'FrodoAragorn' ? 1 : 0); }\n");
+
+        Iterator<Tuple> iter = pig.openIterator("c");
+        String[] expected = new String[]{"(FrodoAragorn,1)", "(SamAragorn,0)", "(GandalfAragorn,0)"};
+
+        Util.checkQueryOutputsAfterSortRecursive(iter, expected,
+                org.apache.pig.newplan.logical.Util.translateSchema(pig.dumpSchema("c")));
+
+        FileSystem.getLocal(new Configuration()).delete(new Path("one_data"), true);
+
+    }
+
+    /*@Test
+    public void testGroupByWithinNested() throws Exception {
+
+        pig.registerQuery("a = load 'my_data' as (col1, col2, col3, col4);\n");
+        pig.registerQuery("b = foreach a { c = grou
+                + "c1 = UPPER((chararray)col2); generate (c1 eq 'FRODO' ? 1 : 0), (c1 eq 'SAM' ? 2 : 0); }\n");
+    }
+*/
+    @Test
+    public void testConstantArg() throws Exception {
+
+        pig.registerQuery("a = load 'my_data' as (col1:int, col2:chararray, col3:chararray, col4:chararray);\n");
+        pig.registerQuery("b = foreach a { c1 = STARTSWITH(TOTUPLE(col2,'f')); generate (c1 eq true ? 'frodo' : 'not frodo'), (c1 eq true ? 'frodo' : 'not frodo'); }\n");
+
+        Iterator<Tuple> iter = pig.openIterator("b");
+        String[] expected = new String[]{"(frodo,not sam)", "(not frodo,sam)", "(not frodo,not sam)"};
+
+        Util.checkQueryOutputsAfterSortRecursive(iter, expected,
+                org.apache.pig.newplan.logical.Util.translateSchema(pig.dumpSchema("b")));
+    }
+
 }
